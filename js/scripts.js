8143504727ebe70a048c47974d4ce7d2f052e7a8
Ross Fletcher and Ryan Ferris <ross@>
2017-12-11 08:57:24 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// BACK END LOGIC
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+
+
+// FRONT END LOGIC
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    // $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  }); // End of 'form submit' event listener.
+}); // End of 'document ready' function.
